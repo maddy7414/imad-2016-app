2a7b61d202451423b9419ff53bca235b816c275b
maddy7414 <madhaw_anand@srmuniv.edu.in>
2016-09-29 20:16:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('this is served 1');
+});
+app.get('/article-two', function (req, res) {
+  res.send('this is served 2');
+});
+app.get('/article-three', function (req, res) {
+  res.send('this is served 3');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
